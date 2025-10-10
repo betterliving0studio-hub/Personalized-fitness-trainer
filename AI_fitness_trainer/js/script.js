7c23dfd2f4dbf7167174602f1bdc5aa7d686a39d
betterliving0studio-hub <betterliving0studio@gmail.com>
2025-10-10 22:05:17 +0500

    Delete AI_fitness_trainer directory

--- AI_fitness_trainer/js/script.js
+++
@@ -1,8 +0,0 @@
-// Smooth scrolling for nav links
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener("click", function(e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute("href"))
-      .scrollIntoView({ behavior: "smooth" });
-  });
-});
